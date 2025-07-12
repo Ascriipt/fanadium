3ab8d458dbda1bd178c6a4aa44f5daaa3916803a
ebedoise <emilebedoiseau@gmail.com>
2025-07-12 21:31:31 +0200

    Fusee Ariane:connect wallet socios, create URI, mint logic, missing wallet info to mint

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MintNFTForm from './components/MintNFTForm';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Connecte ton Socios Wallet</h1>
+      <appkit-button />
+      <MintNFTForm />
+    </div>
+  );
+}
